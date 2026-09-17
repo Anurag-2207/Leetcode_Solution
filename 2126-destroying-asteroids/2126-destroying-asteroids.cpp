d771e3a6d2fa46e8e4cd04928d094dd7cc97176b
Anurag-2207 <anuabhi220724@gmail.com>
2026-09-17 11:55:55 +0530

    Time: 30 ms (64.06%), Space: 106.7 MB (82.07%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& a) {
+        sort(a.begin(),a.end());
+        long long x=mass;
+        for(int i=0;i<a.size();i++){
+            if(x>=a[i]){
+                x+=a[i];
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
